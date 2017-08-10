5727eac554025bb91007d7c1059bc36dc5055c3c
Bolot Kerimbaev <bolot@bignerdranch.com>
2017-08-09 22:43:13 -0400

    Update to ktor 0.4.0-alpha14, use ktor-gson

--- src/main/kotlin/Who.kt
+++ src/main/kotlin/Who.kt
@@ -1,10 +1,9 @@
-import com.google.gson.Gson
 import org.jetbrains.ktor.application.Application
 import org.jetbrains.ktor.application.install
+import org.jetbrains.ktor.features.CallLogging
 import org.jetbrains.ktor.features.DefaultHeaders
-import org.jetbrains.ktor.http.ContentType
-import org.jetbrains.ktor.logging.CallLogging
-import org.jetbrains.ktor.response.respondText
+import org.jetbrains.ktor.gson.GsonSupport
+import org.jetbrains.ktor.response.respond
 import org.jetbrains.ktor.routing.Routing
 import org.jetbrains.ktor.routing.get
 
@@ -13,12 +12,11 @@ data class Who(val name: String, val planet: String)
 fun Application.main() {
     install(DefaultHeaders)
     install(CallLogging)
+    install(GsonSupport)
     install(Routing) {
         get("/") {
             val doktor = Who("Doktor", "Gallifrey")
-            val gson = Gson()
-            val json = gson.toJson(doktor)
-            call.respondText(json, ContentType.Application.Json)
+            call.respond(doktor)
         }
     }
 }
