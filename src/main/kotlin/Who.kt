c6bb5021c1cb41cf31249ecbe01d7ee776adacc4
Bolot Kerimbaev <bolot@bignerdranch.com>
2017-09-11 11:42:32 -0400

    Upgrade to Kotlin 1.1.4-3, explicit Json conversion

--- src/main/kotlin/Who.kt
+++ src/main/kotlin/Who.kt
@@ -1,9 +1,11 @@
+import com.google.gson.Gson
 import org.jetbrains.ktor.application.Application
 import org.jetbrains.ktor.application.install
 import org.jetbrains.ktor.features.CallLogging
 import org.jetbrains.ktor.features.DefaultHeaders
 import org.jetbrains.ktor.gson.GsonSupport
-import org.jetbrains.ktor.response.respond
+import org.jetbrains.ktor.http.ContentType
+import org.jetbrains.ktor.response.respondText
 import org.jetbrains.ktor.routing.Routing
 import org.jetbrains.ktor.routing.get
 
@@ -16,7 +18,9 @@ fun Application.main() {
     install(Routing) {
         get("/") {
             val doktor = Who("Doktor", "Gallifrey")
-            call.respond(doktor)
+            val gson = Gson()
+            val json = gson.toJson(doktor)
+            call.respondText(json, ContentType.Application.Json)
         }
     }
 }
